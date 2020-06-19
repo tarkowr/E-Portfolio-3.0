35bf0ab3048beebb53b873f243b4023d3a726166
Richie Tarkowski <tarkowr@gmail.com>
2020-06-18 22:19:50 -0400

    added bootstrap

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,9 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Landing.css';
 
+const name = <div className="name">Richie Tarkowski</div>;
+const title = <div className="title px-2">Studying Information Systems</div>;
+
 const Landing = () => (
-  <div className="Landing">
-    Landing Component
+  <div className="Landing w-100 bg-dark text-center text-white pt-5">
+    <div className="mt-5">
+    { name }
+    { title }
+    </div>
   </div>
 );
 
