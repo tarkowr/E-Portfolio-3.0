64eb31c194cc47b2f869b0a0631843b1805df614
Richie Tarkowski <tarkowr@gmail.com>
2020-06-23 21:16:38 -0400

    Typed title working

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import Typed from 'react-typed';
 import './Landing.css';
 
-const name = <div className="name">Richie Tarkowski</div>;
-const title = <div className="title px-2">Studying Information Systems</div>;
+export default class Landing extends React.Component {
+  name = <div className="name">Richie Tarkowski</div>;
+  title = (
+    <div className="title px-2">
+      <Typed strings={['Studying Information Systems', 'Hagerty Software Engineer Intern']}
+        typeSpeed={15} 
+        startDelay={0}
+        autoInsertCss={true}
+        fadeOut={false}
+        backDelay={2500}
+        loop={true} />
+    </div>);
 
-const Landing = () => (
-  <div className="Landing w-100 bg-dark text-center text-white">
-    { name }
-    { title }
-  </div>
-);
-
-Landing.propTypes = {};
-
-Landing.defaultProps = {};
-
-export default Landing;
+  render() {
+    return (
+      <div className="Landing w-100 bg-dark text-center text-white">
+        { this.name }
+        { this.title }
+      </div>
+    );
+  }
+}
