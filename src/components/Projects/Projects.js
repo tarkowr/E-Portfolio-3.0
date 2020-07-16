6825d8654749bf021e0a0664dced65d641c93fae
Richie Tarkowski <tarkowr@gmail.com>
2020-07-15 20:13:33 -0400

    refactoring project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -15,12 +15,40 @@ export default class Projects extends React.Component {
   }
 
   getProjectList() {
-    return [
-      <ProjectCard key={0} title='Utility' techList={[<TechCard key={0} title='Android' />, <TechCard key={1} title='Java' />, <TechCard key={2} title='Web API' />]}
-        description='Wrote an Android Utility app that features several programs. Used asynchronous tasks in Java, a SQLite database, and a web API.'
-        lastUpdated={this.state.utility?.['updated_at'] ?? null} actionList={[<ProjectButton key={0} title='GITHUB' icon={<i className="fa fa-code"></i>} link='https://github.com/tarkowr/Utility-App' />, 
-        <ProjectButton key={1} title='DOWNLOAD' icon={<i className="fa fa-arrow-down"></i>} link='https://play.google.com/store/apps/details?id=com.rt.utility' />]} />
+    const githubIcon = <i className="fa fa-code"></i>;
+    const downloadIcon = <i className="fa fa-arrow-down"></i>;
+
+    let utilityTech = ['Android', 'Java', 'Web API'];
+    let utilityActions = [
+      {title: 'GITHUB', icon: githubIcon, link: 'https://github.com/tarkowr/Utility-App'},
+      {title: 'DOWNLOAD', icon: downloadIcon, link: 'https://play.google.com/store/apps/details?id=com.rt.utility'},
     ];
+
+    return [
+      <ProjectCard key={0} title='Utility' techList={this.buildTechCards(utilityTech)}
+        description='Wrote an Android Utility app that features several programs. Used asynchronous tasks in Java, a SQLite database, and a web API.'
+        lastUpdated={this.getLastUpdated(this.state.utility)} actionList={this.buildProjectActions(utilityActions)} />
+    ];
+  }
+
+  buildTechCards(techTitles = []) {
+    let techList = [];
+
+    for (let i=0; i<techTitles.length; i++) {
+      techList.push(<TechCard key={i} title={techTitles[i]} />);
+    }
+
+    return techList;
+  }
+
+  buildProjectActions(projectActions = []) {
+    let actions = [];
+
+    for (let i=0; i<projectActions.length; i++) {
+      actions.push(<ProjectButton key={i} title={projectActions[i].title} icon={projectActions[i].icon} link={projectActions[i].link} />);
+    }
+
+    return actions;
   }
 
   fetchGitHubProjectData() {
@@ -28,14 +56,25 @@ export default class Projects extends React.Component {
 
     axios.get(url)
     .then(response => {
-      this.getProjectLastUpdated(response.data);
+      this.getProjectData(response.data);
     })
     .catch(error => {
       console.log(error);
     });
   }
 
-  getProjectLastUpdated(data) {
+  getLastUpdated(projectData) {
+    if (!projectData) return null;
+
+    let repoLastUpdated = new Date(projectData.updated_at);
+    let date = repoLastUpdated.getDate();
+    let month = repoLastUpdated.getMonth();
+    let year = repoLastUpdated.getFullYear();
+
+    return (month + 1) + '-' + date + '-' + year;
+  }
+
+  getProjectData(data) {
     this.setState({
       utility: this.parseGitHubProject(data, 'Utility-App')
     });
