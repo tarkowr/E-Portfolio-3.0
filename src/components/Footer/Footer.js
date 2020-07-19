a45f9cc61d59f7f33f927249f42b3c3850c7f49f
Richie Tarkowski <tarkowr@gmail.com>
2020-07-19 14:43:10 -0400

    added footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,14 @@ import './Footer.css';
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>
-        Footer Component
-      </div>
+      <div className="my-5 pt-5 pb-3 text-center">
+        <p>
+          <a className="footer-icon" target="_blank" href="https://github.com/tarkowr"><i className="fa fa-github"> </i></a>
+          <a className="footer-icon" target="_blank" href="https://www.linkedin.com/in/richie-tarkowski-273238155"><i className="fa fa-linkedin-square"> </i></a>
+          <a className="footer-icon" target="_blank" href="https://trailhead.com/me/rtarkowski"><i className="fa fa-cloud"> </i></a>
+        </p>
+        <p className="footer-email mt-4">You can reach me a<span> <a href="mailto:tarkowr@gmail.com">tarkowr@gmail.com</a> </span></p>
+      </div> 
     );
   }
 }
