89696a70c0e055160cecc96811f636c38e610438
Richie Tarkowski <tarkowr@gmail.com>
2020-07-23 22:04:21 -0400

    fixed typo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default class Footer extends React.Component {
         <a className="footer-icon mx-2" target="_blank" rel="noopener noreferrer" href="https://github.com/tarkowr"><i className="fa fa-github"> </i></a>
         <a className="footer-icon mx-2" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/richie-tarkowski-273238155"><i className="fa fa-linkedin-square"> </i></a>
         <a className="footer-icon mx-2" target="_blank" rel="noopener noreferrer" href="https://trailhead.com/me/rtarkowski"><i className="fa fa-cloud"> </i></a>
-        <div className="footer-email mt-4">You can reach me a<span> <a href="mailto:tarkowr@gmail.com">tarkowr@gmail.com</a> </span></div>
+        <div className="footer-email mt-4">You can reach me at<span> <a href="mailto:tarkowr@gmail.com">tarkowr@gmail.com</a> </span></div>
       </div> 
     );
   }
