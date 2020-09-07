24901d6f5e0a9b7adb9402edf8ca058b25f863b9
Richie Tarkowski <tarkowr@gmail.com>
2020-09-07 10:19:42 -0500

    updated resume and about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,11 +9,11 @@ export default class About extends React.Component {
           <div className="h1">&mdash; About Me &mdash;</div>
           <div className="row mx-0">
             <div className="text-justify m-auto pt-4 col-12 col-md-10 col-xl-8 px-4 description">
-                My name is Richie Tarkowski, and I am a third-year student at Ferris State University. I am pursuing a degree in Computer Information Systems with a minor in Information Security and Intelligence. I currently work at Hagerty as a Software Engineer Intern. Post-education, my goal is to apply my knowledge of CIS and ISIN to design and develop applications. I have experience in Python, Java, and C# from my classes at Ferris and projects of my own. I've also built websites with several frameworks including Angular, ASP.NET MVC, and Django, and written server-side scripts with NodeJS and PHP. In my free time, I enjoy reading, exercising, investing, and learning.
+                My name is Richie Tarkowski, I am a senior at Ferris State University studying Information Systems with a minor in Information Security, and I live in  the Peoria, IL area. I currently work remotely at Hagerty (Traverse City, MI) as a Software Engineer Intern on its mobile team. Post-education, my goal is to apply my knowledge of CIS and ISIN to design and develop secure software applications. I have experience in Python, Java, Dart, JavaScript, and C# from college, work, and projects of my own. I've also built websites with several frameworks and languages including Angular, React, ASP.NET MVC, NodeJS, and PHP. I've worked with both relational databases (MSSQL and MySQL) and document databases (MongoDB and Cloud Firestore). In my free time, I enjoy reading, exercising, investing, and learning.
             </div>
           </div>
           <div className="mt-5">
-            <a href="/Tarkowski_Resume.pdf" target="_blank" rel="noopener noreferrer"><button className="custom-btn">View Resume</button></a>
+            <a href="/tarkowski_resume.pdf" target="_blank" rel="noopener noreferrer"><button className="custom-btn">View Resume</button></a>
           </div>
         </div>
       </div>
