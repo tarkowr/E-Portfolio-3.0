839d82e0380624ba8569a84e2c5a1f9beb4030e5
Richie Tarkowski <tarkowr@gmail.com>
2020-10-21 21:19:39 -0500

    updated more mobile styles

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -7,7 +7,7 @@ export default class Landing extends React.Component {
   name = <div className="name">Richie Tarkowski</div>;
   title = (
     <div className="title px-2">
-      <Typed strings={['Studying Information Systems', 'Hagerty Software Engineer Intern']}
+      <Typed strings={['Studying Information Systems', 'Hagerty Software Engineer']}
         typeSpeed={15} 
         startDelay={0}
         autoInsertCss={true}
